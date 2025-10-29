868e6e62abe58113694a4d15c47beda0670b5e4e
Dowon Cha <dowoncha.cs@gmail.com>
2025-10-29 15:26:01 -0400

    add header

--- apps/frontend/app/(default)/layout.tsx
+++ apps/frontend/app/(default)/layout.tsx
@@ -1,9 +1,18 @@
 import { ResumePreviewProvider } from '@/components/common/resume_previewer_context';
+import Link from 'next/link';
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <ResumePreviewProvider>
-      <main className="min-h-screen flex flex-col">{children}</main>
+      <main className="min-h-screen flex flex-col">
+        <header className="bg-gray-800 relative">
+          <nav className="mx-auto max-w-7xl h-16 text-white flex items-center gap-4">
+            <div className="text-2xl font-bold cursor-pointer">
+              <Link href="/">GovStar Resume Matcher</Link>
+            </div>
+          </nav>
+        </header>
+        {children}</main>
     </ResumePreviewProvider>
   );
 }
